3877c9e2e6bc985d420886db53432caee5186650
Hector Jusforgues <hector@sogilis.com>
2019-09-11 14:03:07 +0200

    Add type and dateTime to event

--- src/EventStore/Event.php
+++ src/EventStore/Event.php
@@ -2,13 +2,27 @@
 
 namespace App\EventStore;
 
+use DateTimeImmutable;
+
 class Event
 {
     private $data = [];
 
-    public function __construct(array $data)
+    /**
+     * @var string
+     */
+    private $type;
+
+    /**
+     * @var DateTimeImmutable
+     */
+    private $dateTime;
+
+    public function __construct(string $type, array $data, DateTimeImmutable $dateTime)
     {
         $this->data = $data;
+        $this->type = $type;
+        $this->dateTime = $dateTime;
     }
 
     public function toJson(): array
